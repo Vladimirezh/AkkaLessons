6e0376e8d59fce96a5d9fda5a2210f370edb66b5
Vladimir <v-ezhikov@armo.ru>
2015-09-12 13:33:44 +0300

    Lesson2

---
+++ Lesson2/Actors/ConsoleReaderActor.cs
@@ -0,0 +1,55 @@
+﻿using Akka.Actor;
+using System;
+
+namespace Lesson2.Actors
+{
+    public class ConsoleReaderActor : UntypedActor
+    {
+        private IActorRef consoleWriter;
+        public static string StartCommand = "start";
+        public static string ExitCommand = "end";
+
+        public ConsoleReaderActor(IActorRef writer)
+        {
+            consoleWriter = writer;
+        }
+
+        protected override void OnReceive(object message)
+        {
+            if (message.Equals(StartCommand))
+            {
+                Console.WriteLine("Reader started.");
+                Console.WriteLine("Write message started with # and press enter.");
+            }
+            else if (message is Messages.InputError)
+            {
+                consoleWriter.Tell(message);
+            }
+            ReadAndValidateMessage();
+        }
+
+        private void ReadAndValidateMessage()
+        {
+            var readedLine = Console.ReadLine();
+            if (string.IsNullOrEmpty(readedLine))
+            {
+                Self.Tell(new Messages.InputError("Empty or null string"));
+            }
+            else if (readedLine.Equals(ExitCommand))
+            {
+                Context.System.Shutdown();
+
+            }
+            else if (readedLine.StartsWith("#"))
+            {
+                consoleWriter.Tell(new Messages.InputSuccess("Thank you! Message was valid."));
+                Self.Tell(new Messages.ContinueProcessing());
+            }
+            else
+            {
+                Self.Tell(new Messages.ValidationError("String doesnt start with #"));
+            }
+
+        }
+    }
+}
---
+++ Lesson2/Program.cs
@@ -0,0 +1,23 @@
+﻿using Akka.Actor;
+using Lesson2.Actors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var system = ActorSystem.Create("TestSystem");
+            var writer = system.ActorOf(Props.Create((() => new Actors.ConsoleWriterActor())), "Writer");
+            var reader = system.ActorOf(Props.Create(typeof(Actors.ConsoleReaderActor), writer), "Reader");
+            reader.Tell(ConsoleReaderActor.StartCommand);
+            system.AwaitTermination();
+            Console.ReadKey();
+        }
+    }
+}
